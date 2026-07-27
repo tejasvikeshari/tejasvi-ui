ab7f3d5bb89f83fdc15c16d9c164244139296a71
Tejasvi Kesharwani <tejasvikeshari0708@gmail.com>
2026-07-27 23:19:53 +0530

    Configured GitHub Pages

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/tejasvi-ui/',
+
   plugins: [react()],
+
   build: {
     outDir: 'dist',
     sourcemap: false,
   },
+
   server: {
     port: 5173,
     open: true,
